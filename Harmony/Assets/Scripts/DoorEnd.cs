43ca14a79b12998c252c8e2d2b2ce8b45ce1bd1a
livisvilis <livis.vilis@gmail.com>
2022-11-27 15:21:58 +0100

    adding end level anim

--- Harmony/Assets/Scripts/DoorEnd.cs
+++ Harmony/Assets/Scripts/DoorEnd.cs
@@ -16,6 +16,7 @@ public class DoorEnd : MonoBehaviour
     public TMPro.TextMeshProUGUI timerText;
     string minutes;
     string seconds;
+    private GameObject logo;
 
     // Start is called before the first frame update
     void Start()
@@ -26,6 +27,8 @@ public class DoorEnd : MonoBehaviour
         endLevel = GameObject.Find("CanvasEndLevel").GetComponent<Canvas>();
         jin = GameObject.FindGameObjectWithTag("jin");
         jang = GameObject.FindGameObjectWithTag("jang");
+        logo = GameObject.Find("logo");
+        logo.SetActive(false);
     }
 
     // Update is called once per frame
@@ -34,24 +37,38 @@ public class DoorEnd : MonoBehaviour
         //dW.touchingWhite
         if (dW.touchingWhite == true && dB.touchingBlack == true)
         {
-            endLevel.enabled = true;
-            t.stopwatchActive = false;
-            TimeSpan time = TimeSpan.FromSeconds(t.currentTime);
-            if (time.Minutes < 10)
-            {
-                minutes = "0" + time.Minutes.ToString();
-            }
-            else { minutes = time.Minutes.ToString(); }
-            if (time.Seconds < 10)
-            {
-                seconds = "0" + time.Seconds.ToString();
-            }
-            else { seconds = time.Seconds.ToString(); }
-            //currentTimeText.text = time.Minutes.ToString() + ":" + time.Seconds.ToString();
-            timerText.text = minutes + ":" + seconds;
-
             jin.gameObject.SetActive(false);
             jang.gameObject.SetActive(false);
+            dB.gameObject.SetActive(false);
+            dW.gameObject.SetActive(false);
+
+            StartCoroutine("EndLevel");
+
+            
+            t.stopwatchActive = false;
+            
+
+            
         }
+    }
+    public IEnumerator EndLevel()
+    {
+        logo.SetActive(true);
+        yield return new WaitForSeconds(2.5f);
+        endLevel.enabled = true;
+        TimeSpan time = TimeSpan.FromSeconds(t.currentTime);
+        if (time.Minutes < 10)
+        {
+            minutes = "0" + time.Minutes.ToString();
+        }
+        else { minutes = time.Minutes.ToString(); }
+        if (time.Seconds < 10)
+        {
+            seconds = "0" + time.Seconds.ToString();
+        }
+        else { seconds = time.Seconds.ToString(); }
+        //currentTimeText.text = time.Minutes.ToString() + ":" + time.Seconds.ToString();
+        timerText.text = minutes + ":" + seconds;
+        yield break;
     }
 }
